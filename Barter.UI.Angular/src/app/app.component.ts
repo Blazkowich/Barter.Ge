3474d678f76328c66d8884d277cecc1e01391dd0
Otar <otar_iluridze@epam.com>
2024-05-04 22:42:19 +0400

    Fixed View of Categories

--- Barter.UI.Angular/src/app/app.component.ts
+++ Barter.UI.Angular/src/app/app.component.ts
@@ -1,16 +1,16 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Category } from './models/category.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
   categories: Category[] = [];
 
-  constructor(private http: HttpClient, private cdr: ChangeDetectorRef) { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
     this.getCategories();
@@ -21,7 +21,6 @@ export class AppComponent implements OnInit {
       next: (categories) => {
         this.categories = categories;
         console.log('Categories:', this.categories);
-        this.cdr.detectChanges();
       },
       error: (error) => {
         console.error('Error fetching categories:', error);
